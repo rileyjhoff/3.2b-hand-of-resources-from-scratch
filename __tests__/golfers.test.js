aab60bd4eabf0f08449a84d1a017798c96a5d2a1
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-15 16:01:00 -0700

    add GET /golfers route

--- __tests__/golfers.test.js
+++ __tests__/golfers.test.js
@@ -7,8 +7,16 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /golfers returns list of golfers', async () => {
+    const resp = await request(app).get('/golfers');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual([
+      { id: 1, name: 'Will Zalatoris', ranking: 14, wins: 1, age: 25 },
+      { id: 2, name: 'Sam Burns', ranking: 9, wins: 5, age: 25 },
+      { id: 3, name: 'Max Homa', ranking: 23, wins: 6, age: 31 },
+      { id: 4, name: 'Brooks Koepka', ranking: 19, wins: 15, age: 32 },
+      { id: 5, name: 'Davis Riley', ranking: 85, wins: 2, age: 25 },
+    ]);
   });
   afterAll(() => {
     pool.end();
