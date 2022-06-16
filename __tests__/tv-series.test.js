2f4969790f4dbd7d3608b60fa2547c24682f693e
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-16 15:19:47 -0700

    add GET /tv-series route with test

--- __tests__/tv-series.test.js
+++ __tests__/tv-series.test.js
@@ -8,8 +8,47 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /tv-series should return a list of TV Series', async () => {
+    const resp = await request(app).get('/tv-series');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual([
+      {
+        id: 1,
+        title: 'Barry',
+        genre: ['Action', 'Comedy', 'Crime'],
+        original_network: 'HBO',
+        seasons: 3,
+        imdb_rating: 8,
+        rt_rating: '99%',
+      },
+      {
+        id: 2,
+        title: 'Obi-Wan Kenobi',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
+        original_network: 'Disney+',
+        seasons: 1,
+        imdb_rating: 7,
+        rt_rating: '84%',
+      },
+      {
+        id: 3,
+        title: 'Severance',
+        genre: ['Drama', 'Mystery', 'Sci-Fi'],
+        original_network: 'Apple TV',
+        seasons: 1,
+        imdb_rating: 9,
+        rt_rating: '98%',
+      },
+      {
+        id: 4,
+        title: 'South Park',
+        genre: ['Animation', 'Comedy'],
+        original_network: 'Comedy Central',
+        seasons: 25,
+        imdb_rating: 9,
+        rt_rating: '80%',
+      },
+    ]);
   });
 
   afterAll(() => {
