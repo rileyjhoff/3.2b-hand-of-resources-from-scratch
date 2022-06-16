412ac46183bdc04fc37607b16a34194a94b3981c
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-15 17:34:21 -0700

    seed teams table, update file names, update index.html

--- sql/setup.sql
+++ sql/setup.sql
@@ -23,3 +23,23 @@ VALUES
   ('Max Homa', 23, 6, 31),
   ('Brooks Koepka', 19, 15, 32),
   ('Davis Riley', 85, 2, 25);
+
+DROP TABLE IF EXISTS teams;
+
+CREATE TABLE teams (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  league VARCHAR NOT NULL,
+  sport VARCHAR NOT NULL
+);
+
+INSERT INTO teams (
+  name, 
+  league,
+  sport
+  ) 
+VALUES 
+  ('Portland Trail Blazers', 'NBA', 'basketball'),
+  ('Colorado Avalanche', 'NHL', 'hockey'),
+  ('Portland Timbers', 'MLS', 'soccer'),
+  ('Chelsea FC', 'EPL', 'soccer');
