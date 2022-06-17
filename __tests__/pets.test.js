410be9d48e35aff5073758653a2ce35575341239
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-16 17:41:46 -0700

    add GET /pets/:id route with test

--- __tests__/pets.test.js
+++ __tests__/pets.test.js
@@ -39,6 +39,17 @@ describe('backend-express-template routes', () => {
     ]);
   });
 
+  it('GET /pets/:id should return pet detail', async () => {
+    const resp = await request(app).get('/pets/4');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: 4,
+      name: 'Otter',
+      age: 0,
+      type: 'cat',
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
